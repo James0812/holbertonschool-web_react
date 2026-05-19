ad6f02979b125419bb7e1a8d9f765e7dd31dc05b
James0812 <james.roussel008@gmail.com>
2026-05-19 10:36:34 +0200

    add the code

--- TypeScript/task_4/js/main.ts
+++ TypeScript/task_4/js/main.ts
@@ -1,34 +1,42 @@
+import "./subjects/Teacher";
+import "./subjects/Subject";
+import "./subjects/Cpp";
+import "./subjects/React";
+import "./subjects/Java";
+
 /// <reference path="./subjects/Teacher.ts" />
 /// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/React.ts" />
 /// <reference path="./subjects/Java.ts" />
 
-namespace Subjects {
-  export const teacher: Teacher = {
-    firstName: "Guillaume",
-    lastName: "Salva",
-    experienceTeachingC: 10,
-    experienceTeachingReact: 5,
-    experienceTeachingJava: 2,
-  };
+// Création du teacher
+const teacher: Subjects.Teacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10,
+  experienceTeachingReact: 5,
+  experienceTeachingJava: 2,
+};
 
-  const cpp = new Cpp();
-  cpp.setTeacher(teacher);
-  console.log("Cpp:");
-  console.log(cpp.getRequirements());
-  console.log(cpp.getAvailableTeacher());
+// Cpp
+const cpp = new Subjects.Cpp();
+cpp.setTeacher(teacher);
+console.log("Cpp:");
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
 
-  const react = new React();
-  react.setTeacher(teacher);
-  console.log("React:");
-  console.log(react.getRequirements());
-  console.log(react.getAvailableTeacher());
+// React
+const react = new Subjects.React();
+react.setTeacher(teacher);
+console.log("React:");
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
 
-  const java = new Java();
-  java.setTeacher(teacher);
-  console.log("Java:");
-  console.log(java.getRequirements());
-  console.log(java.getAvailableTeacher());
-}
+// Java
+const java = new Subjects.Java();
+java.setTeacher(teacher);
+console.log("Java:");
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
 
