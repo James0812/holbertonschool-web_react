2200cf3228782ca4f0f2ae31f3506b99fdc7b490
James0812 <james.roussel008@gmail.com>
2026-05-19 10:30:56 +0200

    add the code

--- TypeScript/task_4/js/main.ts
+++ TypeScript/task_4/js/main.ts
@@ -1,3 +1,4 @@
+// Force Webpack à inclure les fichiers
 import "./subjects/Teacher";
 import "./subjects/Subject";
 import "./subjects/Cpp";
@@ -11,6 +12,7 @@ import "./subjects/Java";
 /// <reference path="./subjects/Java.ts" />
 
 namespace Subjects {
+  // Création d’un teacher
   export const teacher: Teacher = {
     firstName: "Guillaume",
     lastName: "Salva",
@@ -19,18 +21,21 @@ namespace Subjects {
     experienceTeachingJava: 2,
   };
 
+  // Cpp
   const cpp = new Cpp();
   cpp.setTeacher(teacher);
   console.log("Cpp:");
   console.log(cpp.getRequirements());
   console.log(cpp.getAvailableTeacher());
 
+  // React
   const react = new React();
   react.setTeacher(teacher);
   console.log("React:");
   console.log(react.getRequirements());
   console.log(react.getAvailableTeacher());
 
+  // Java
   const java = new Java();
   java.setTeacher(teacher);
   console.log("Java:");
